eb01221950e1d5874806a942637fac633fcb61e2
Moran0103 <110515247+Moran0103@users.noreply.github.com>
2024-04-16 18:03:57 -0400

    Update CCommandNode.cpp

--- Prj/p1/psh/src/ast/CCommandNode.cpp
+++ Prj/p1/psh/src/ast/CCommandNode.cpp
@@ -1,7 +1,7 @@
 /**
- * @author Qucheng Jiang
- * @author jiang.qu@northeastern.edu
- * @author NUID 001569593
+ * @author Qucheng Jiang / Shihan Zhao
+ * @author jiang.qu@northeastern.edu / zhao.shiha@northeastern.edu
+ * @author NUID 001569593 / 002772845
  */
 
 #include "ast/CCommandNode.h"
